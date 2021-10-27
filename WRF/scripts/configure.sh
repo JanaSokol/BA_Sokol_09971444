637a1bb39e3a5158391087c58140a280f0103a4a
JanaSokol <e9971444@student.tuwien.ac.at>
2021-10-27 11:19:36 +0200

    Updated main and configure script, added jasper tar.gz file

--- WRF/scripts/configure.sh
+++ WRF/scripts/configure.sh
@@ -1,12 +1,184 @@
 #!/bin/bash
 
+
+#################################
+# directory paths
+#################################
 cd ..
 WRFROOT=`pwd`
-DOWNLOADSDIR=$WRFROOT/downloads/
+DOWNLOADSDIR=$WRFROOT/downloads
+LIBSDIR=$WRFROOT/libs
+WRFDIR=$WRFROOT/WRF
 
+#################################
+# unzip dependencies, WRF and WPS
+#################################
+cd $DOWNLOADSDIR
+#for i in *.gz ; do tar xzf $i ; done
+#mv -v WPS-4.3/ ../WPS
+#mv -v WRF-4.3/ ../WRF
+cd geog_high_res_mandatory/
+mv -v WPS_GEOG/ ../../GEOG
 
+#################################
+# installing necessary packages 
+#################################
+echo "********** installing necessary packages **********"
+installed() {
+    return $(dpkg-query -W -f '${Status}\n' "${1}" 2>&1|awk '/ok installed/{print 1;exit}{print 0}')
+}
 
-for i in *.gz ; do tar xzf $i ; done
-for i in *.zip ; do tar xzf $i ; done
+#gfortran
+if installed gfortran; then
+    sudo apt install gfortran
+else
+    echo "gfortran is already installed."
+fi
+
+#g++
+if installed g++; then
+    sudo apt install g++
+else
+    echo "g++ is already installed."
+fi
+
+#htop
+if installed htop; then
+    sudo apt install htop
+else
+    echo "htop is already installed."
+fi
+
+#csh
+if installed csh; then
+    sudo apt install csh
+else
+    echo "csh is already installed."
+fi
+
+#mc
+if installed mc; then
+    echo "This is optional, do you want to install mc? Midnight Commander is a text-mode full-screen file manager."
+    read -p '[Y/N]' answervar
+    if [$answervar == 'Y'] || [$answervar == 'y']; then
+        sudo apt install mc
+    fi
+else
+    echo "This is optional, do you want to install mc? Midnight Commander is a text-mode full-screen file manager."
+    read -p '[Y/N]' answervar
+    if [$answervar == 'Y'] || [$answervar == 'y']; then
+        sudo apt install mc
+    fi
+    echo "mc is already installed."
+fi
+
+#################################
+# creating folder hierarchy
+#################################
+cd $WRFROOT
+if [ ! -d "$WRFROOT/GFS" ]; then
+    mkdir GFS
+fi
+if [ ! -d "$LIBSDIR" ]; then
+    mkdir libs
+    cd libs
+    mkdir grib2 mpich netcdf
+else
+    cd $LIBSDIR
+    if [ ! -d "$WRFROOT/libs/grib2" ]; then
+        mkdir grib2
+    fi
+
+    if [ ! -d "$WRFROOT/libs/mpich" ]; then
+        mkdir mpich
+    fi
+
+    if [ ! -d "$WRFROOT/libs/netcdf" ]; then
+        mkdir netcdf
+    fi
+fi
 
 export LIBDIR=$WRFROOT/libs
+
+#################################
+# zlib
+#################################
+#cd $DOWNLOADSDIR/zlib-1.2.11
+#./configure --prefix=$LIBDIR/grib2 
+#make 
+#make install
+
+#################################
+# libpng
+#################################
+#cd $DOWNLOADSDIR/libpng-1.6.37
+#./configure --prefix=$LIBDIR/grib2 LDFLAGS="-L$LIBDIR/grib2/lib" CPPFLAGS="-I$LIBDIR/grib2/include"
+#make 
+#make install
+#cd ..
+#rm libpng-1.6.37
+
+#################################
+# jasper
+#################################
+#cd $DOWNLOADSDIR/jasper-1.900.1
+#./configure --prefix=$LIBDIR/grib2
+#make 
+#make install
+#cd ..
+#rm jasper-1.900.1
+
+#################################
+# netcdf
+#################################
+#cd $DOWNLOADSDIR/netcdf-4.1.2
+#./configure --prefix=$LIBDIR/netcdf --disable-dap --disable-netcdf-4
+#make 
+#make install
+#cd ..
+#rm netcdf-4.1.2
+
+#################################
+# mpich
+#################################
+#cd $DOWNLOADSDIR/mpich-3.3.2
+#./configure --prefix=$LIBDIR/mpich
+#make 
+#make install
+#cd ..
+#rm mpich-3.3.2
+
+#################################
+# WRF
+#################################
+#cd $WRFDIR
+#export LIBDIR=$LIBDIR
+#export NETCDF=$LIBDIR/netcdf
+#export PATH=$LIBDIR/mpich/bin:$PATH
+#export JASPERLIB=$LIBDIR/grib2/lib
+#export JASPERINC=$LIBDIR/grib2/include
+#./configure
+#./compile em_real
+#export LD_LIBRARY_PATH=$NETCDF/lib:$LD_LIBRARY_PATH+
+
+#################################
+# WPS
+#################################
+cd $WRFROOT/WPS
+#export WRF_DIR=$WRFDIR
+#./configure
+#./compile
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- WRF/scripts/main.sh
+++ WRF/scripts/main.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+#################################
 # input
+#################################
 STARTYEAR=2021
 STARTDAY=22
 STARTMONTH=10
 STARTHOUR=00
 ENDHOUR=06
 
+#################################
 # enviroment variables
+#################################
 cd ..
 WRFROOT=`pwd`
 WPSDIR=$WRFROOT/WPS
@@ -15,8 +19,13 @@ WRFDIR=$WRFROOT/WRF
 SCRIPTDIR=$WRFROOT/scripts
 CONFIGDIR=$WRFROOT/config
 
-# remove previous created files and logs
+export LIBDIR=$WRFROOT/libs
+export LD_LIBRARY_PATH=$LIBDIR/netcdf/lib:$LD_LIBRARY_PATH
+export PATH=$LIBDIR/mpich/bin:$PATH
 
+#################################
+# remove previous created files
+#################################
 # clean up WPS
 rm -rf $WPSDIR/FILE:*
 rm -rf $WPSDIR/met_em*
@@ -24,7 +33,6 @@ rm -rf $WPSDIR/ungrib.log
 rm -rf $WPSDIR/metgrid.log
 rm -rf $WPSDIR/GRIBFILE.*
 rm -rf $WPSDIR/namelist.wps
-
 # clean up WRF
 rm -rf $WRFDIR/run/wrfout*
 rm -rf $WRFDIR/run/wrfbdy*
@@ -33,13 +41,17 @@ rm -rf $WRFDIR/run/rsl.*
 rm -rf $WRFDIR/run/met_em*
 rm -rf $WRFDIR/run/namelist.input
 
+#################################
 # downloading GFS
+#################################
 echo " " 
 echo "********** Downloading GFS Data **********"
 echo " " 
 bash $SCRIPTDIR/download_gfs.sh $STARTYEAR $STARTMONTH $STARTDAY $STARTHOUR
 
+#################################
 # update namelist.wps
+#################################
 cp $CONFIGDIR/namelist.wps $WPSDIR/namelist.wps
 sed -i "s/STARTYEAR/$STARTYEAR/g" $WPSDIR/namelist.wps
 sed -i "s/STARTDAY/$STARTDAY/g" $WPSDIR/namelist.wps
@@ -51,8 +63,22 @@ sed -i "s/ENDMONTH/$STARTMONTH/g" $WPSDIR/namelist.wps
 sed -i "s/ENDDAY/$STARTDAY/g" $WPSDIR/namelist.wps
 sed -i "s/ENDHOUR/$ENDHOUR/g" $WPSDIR/namelist.wps
 
-# ungrib and metgrid
+#################################
+# geogrid
+#################################
 cd $WPSDIR
+FILE=$WPSDIR/geo_em.d01.nc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else
+    echo "create domain."
+    ./georgrid.exe
+fi
+
+
+#################################
+# ungrib and metgrid
+#################################
 ./link_grib.csh $WRFROOT/GFS/
 echo " "
 echo "********** Running ungrib **********"
@@ -63,7 +89,9 @@ echo "********** Running metgrid **********"
 echo " "
 ./metgrid.exe
 
+#################################
 # update namelist.input
+#################################
 cp $CONFIGDIR/namelist.input $WRFDIR/run/namelist.input
 sed -i "s/STARTYEAR/$STARTYEAR/g" $WRFDIR/run/namelist.input
 sed -i "s/STARTDAY/$STARTDAY/g" $WRFDIR/run/namelist.input
@@ -75,7 +103,9 @@ sed -i "s/ENDDAY/$STARTDAY/g" $WRFDIR/run/namelist.input
 sed -i "s/ENDMONTH/$STARTMONTH/g" $WRFDIR/run/namelist.input
 sed -i "s/ENDHOUR/$ENDHOUR/g" $WRFDIR/run/namelist.input
 
+#################################
 # wrf
+#################################
 cd $WRFDIR/run
 ln -s $WPSDIR/met_em* .
 echo " "
